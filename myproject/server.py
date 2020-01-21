67d79a8c61f39ab8a864752b9b97e28b6197a7fd
WonnieThepoo <51676540+WonnieThepoo@users.noreply.github.com>
2020-01-21 12:52:12 +0300

    +

--- myproject/server.py
+++ myproject/server.py
@@ -15,93 +15,12 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
     return app
 app = create_app()
+db.create_all(app=create_app())
 
 
-
-
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    #cams = db.relationship('TCam', backref='user', lazy=False)
-
-    def __init__(self, email, password):
-        self.email = email
-        self.password = generate_password_hash(password, method='sha256')
-
-    @classmethod
-    def authenticate(cls, **kwargs):
-        email = kwargs.get('email')
-        password = kwargs.get('password')
-
-        if not email or not password:
-            return None
-
-        user = cls.query.filter_by(email=email).first()
-        if not user or not check_password_hash(user.password, password):
-            return None
-
-        return user
-
-    def to_dict(self):
-        return dict(id=self.id,
-                    email=self.email,
-                    cams=[post.to_dict() for post in self.posts] #доделать под себя
-                    )
-
-class TCam(db.Model):
-    __tablename__ = 'tcams'
-
-    uid = db.Column(db.String, unique=True, primary_key=True)
-    ip = db.Column(db.String, nullable=True)
-    port = db.Column(db.Integer, nullable=True)
-    user = db.Column(db.String, nullable=True, default="admin")
-    password = db.Column(db.String, nullable=True, default="Supervisor")
-    name = db.Column(db.String, nullable = True)
-    room =  db.Column(db.String, nullable = True)
-
-    def to_dict(self):
-        return dict(
-            uid = self.uid,
-            ip = self.ip,
-            port = self.ip,
-            user = self.user,
-            passwrod = self.password,
-            name = self.name,
-            room = self.room)
-
-
-class Room(db.Model):
-    __tablename__ = 'rooms'
-
-    id = db.Column(db.String, primary_key=True)
-    cam_name = db.Column(db.String, nullable=True)
-    uid = db.Column(db.String,  db.ForeignKey('cam_table.uid'), unique=True, nullable=True)
-    #cam_table = db.relationship('Cam_table',  backref=db.backref('rooms', lazy=True))
-
-    def to_dict(self):
-        return dict(
-            id = self.id,
-            cam_name = self.cam_name,
-            uid = self.uid,
-            cam_table = self.cam_table
-        )
-
-"""class Cam_table(db.Model):
-    __tablename__ = 'cam_table"""
-
-
-
-
-cam = OCC(("172.18.200.53", 80), "admin", "Supervisor", "wsdl")
+#cam = OCC(("172.18.200.56", 80), "admin", "Supervisor", "wsdl")
 cams = {}
 cams_in_room = {}
 chosen_cam = ''
@@ -138,14 +57,16 @@ def chose_cam():
 @auth_required
 def set_cam():
     data = request.get_json()
-    new_cam = TCam(name=data['name'])
-    new_cam.ip = data['ip']
-    new_cam.port = data['port']
+    new_cam = TCam(uid=data['uid'])
+    new_cam.ip = data['adr']['ip']
+    new_cam.port = data['adr']['port']
     new_cam.user = data['user']
     new_cam.password = data['password']
-    new_cam.room = data['room']
     db.session.add(new_cam)
     db.session.commit()
+
+
+
     return 'ok'
 
 """@app.route('/set_cam', methods=['POST'])
--- myproject/sqldb.py
+++ myproject/sqldb.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
@@ -9,7 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    #cams = db.relationship('TCam', backref='user', lazy=False)
+    cams = db.relationship('TCam', backref='user', lazy=False)
 
     def __init__(self, email, password):
         self.email = email
@@ -41,10 +42,8 @@ class TCam(db.Model):
     uid = db.Column(db.String, unique=True, primary_key=True)
     ip = db.Column(db.String, nullable=True)
     port = db.Column(db.Integer, nullable=True)
-    user = db.Column(db.String, nullable=True, default="admin")
-    password = db.Column(db.String, nullable=True, default="Supervisor")
-    name = db.Column(db.String, nullable = True)
-    room =  db.Column(db.String, nullable = True)
+    user = db.Column(db.String,  default="admin")
+    password = db.Column(db.String, default="Supervisor")
 
     def to_dict(self):
         return dict(
@@ -52,27 +51,36 @@ class TCam(db.Model):
             ip = self.ip,
             port = self.ip,
             user = self.user,
-            passwrod = self.password,
-            name = self.name,
-            room = self.room)
+            passwrod = self.password)
 
 
 class Room(db.Model):
     __tablename__ = 'rooms'
-
-    id = db.Column(db.String, primary_key=True)
+#надо вынести таблицу с комнатами (idr + name комнаты) и ссылаться на неё
+    idr = db.Column(db.String, primary_key=True)
     cam_name = db.Column(db.String, nullable=True)
-    uid = db.Column(db.String,  db.ForeignKey('cam_table.uid'), unique=True, nullable=True)
+
+
     #cam_table = db.relationship('Cam_table',  backref=db.backref('rooms', lazy=True))
 
     def to_dict(self):
         return dict(
-            id = self.id,
+            idr = self.idr,
+            cam_name = self.cam_name,
+            uid = self.uid,
+        )
+
+class Cam_table(db.Model):   #таблица нахождений камер
+    __tablename__ = 'cam_table'
+
+    idr = db.Column(db.String, db.ForeignKey('rooms.idr'), nullable=True)
+    uid = db.Column(db.String,  db.ForeignKey('tcams.uid'), nullable=True,  unique=True)
+    tcams = relationship('Tcams', bakref = 'tcams')
+
+    def to_dict(self):
+        return dict(
+            idr = self.idr,
             cam_name = self.cam_name,
             uid = self.uid,
             cam_table = self.cam_table
         )
-
-"""class Cam_table(db.Model):
-    __tablename__ = 'cam_table"""
-
